e6714d665870ad548afceb7d97fdb5ec532f7bbb
bmbper <rs_bmbp@163.com>
2023-07-25 20:20:19 +0800

    增加模板

--- code/bmbp_app_file/src/lib.rs
+++ code/bmbp_app_file/src/lib.rs
@@ -3,14 +3,12 @@ use salvo::Router;
 
 mod file;
 use file::file_router;
-mod embed;
-use embed::build_embed_router;
 
 pub fn build_file_router() -> Router {
     Router::new()
         .push(Router::with_path("/bmbp/file").push(file_router()))
         .push(
-            Router::with_path("/static/<**path>").get(StaticDir::new(["webapp/lib"]).listing(true)),
+            Router::with_path("/static/<**path>")
+                .get(StaticDir::new(["webapp/static"]).listing(true)),
         )
-        .push(build_embed_router())
 }
