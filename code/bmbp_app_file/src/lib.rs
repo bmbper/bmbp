ecffd1e4fe16c4dfc709748d151c12c3f0631486
bmbper <rs_bmbp@163.com>
2023-08-18 16:00:25 +0800

    fix: prod

--- code/bmbp_app_file/src/lib.rs
+++ code/bmbp_app_file/src/lib.rs
@@ -10,6 +10,6 @@ use file::file_router;
 pub fn build_file_router() -> Router {
     Router::new()
         .push(Router::with_path("/bmbp/file").push(file_router()))
-        .push(Router::with_path("/static/<**path>").get(StaticDir::new(vec!["webapp/static"])))
-    //.push(Router::with_path("/static/<**path>").get(static_embed::<EmbedStatic>()))
+        // .push(Router::with_path("/static/<**path>").get(StaticDir::new(vec!["webapp/static"])))
+        .push(Router::with_path("/static/<**path>").get(static_embed::<EmbedStatic>()))
 }
