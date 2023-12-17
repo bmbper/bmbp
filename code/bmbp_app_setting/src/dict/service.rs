3de86ab58a834dcadb116c7c85808db5f565651c
zhangguokai <zhangguokai@inspur.com>
2023-12-17 17:47:59 +0800

    feat: 字典功能查询接口拼接SQL

--- code/bmbp_app_setting/src/dict/service.rs
+++ code/bmbp_app_setting/src/dict/service.rs
@@ -1,11 +1,14 @@
 use bmbp_app_common::{BmbpResp, BmbpTreeModel};
 use bmbp_app_curd::BmbpCurdScript;
+use bmbp_orm_sql::SqlBuilder;
 use crate::dict::model::{BmbpSettingDictOrmTreeModel, DictQueryParams};
 
 pub struct BmbpSettingDictService;
 
 impl BmbpSettingDictService {
     pub async fn find_dict_tree(params: &DictQueryParams) -> BmbpResp<Vec<BmbpSettingDictOrmTreeModel>> {
+        let query_sql = BmbpCurdScript::query::<BmbpSettingDictOrmTreeModel>();
+        tracing::info!("query_sql: {}", query_sql.build());
         let dict_vec = vec![];
         let tree_node: Vec<BmbpSettingDictOrmTreeModel> = BmbpTreeModel::build_tree_without_spurious(dict_vec);
         return Ok(tree_node);
