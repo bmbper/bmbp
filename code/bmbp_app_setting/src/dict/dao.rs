f62a3867f0e26db2b04c5335b81f38d983c8f866
zhangguokai <zhangguokai@inspur.com>
2023-12-24 22:35:39 +0800

    feat: add orm Dao for dict

--- code/bmbp_app_setting/src/dict/dao.rs
+++ code/bmbp_app_setting/src/dict/dao.rs
@@ -1,1 +1,11 @@
+use bmbp_app_common::BmbpResp;
+use bmbp_orm_ins::BmbpORM;
+use crate::dict::model::{BmbpSettingDict, BmbpSettingDictOrmTreeModel, DictQueryParams};
+
 pub struct BmbpSettingDictDao;
+
+impl BmbpSettingDictDao {
+    pub(crate) async fn query(sql: &String, params: &&DictQueryParams) -> BmbpResp<Vec<BmbpSettingDictOrmTreeModel>>{
+        return Ok(vec![]);
+    }
+}
--- code/bmbp_app_setting/src/dict/service.rs
+++ code/bmbp_app_setting/src/dict/service.rs
@@ -1,6 +1,7 @@
 use bmbp_app_common::{BmbpResp, BmbpTreeModel};
 use bmbp_app_curd::BmbpCurdScript;
 use bmbp_orm_sql::SqlBuilder;
+use crate::dict::dao::BmbpSettingDictDao;
 use crate::dict::model::{BmbpSettingDictOrmTreeModel, DictQueryParams};
 
 pub struct BmbpSettingDictService;
@@ -9,7 +10,7 @@ impl BmbpSettingDictService {
     pub async fn find_dict_tree(params: &DictQueryParams) -> BmbpResp<Vec<BmbpSettingDictOrmTreeModel>> {
         let query_sql = BmbpCurdScript::query::<BmbpSettingDictOrmTreeModel>();
         tracing::info!("query_sql: {}", query_sql.build());
-        let dict_vec = vec![];
+        let dict_vec = BmbpSettingDictDao::query(&query_sql.build(),&params).await?;
         let tree_node: Vec<BmbpSettingDictOrmTreeModel> = BmbpTreeModel::build_tree_without_spurious(dict_vec);
         return Ok(tree_node);
     }
