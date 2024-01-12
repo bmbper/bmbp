e3724a0095899c1a1c5c47b797243e3a617502bd
zhangguokai <zhangguokai@inspur.com>
2024-01-12 14:57:39 +0800

    feat: 增加rdbc模块组，用于替换原来的ORM模块

---
+++ code/bmbp_rdbc/src/driver.rs
@@ -0,0 +1,12 @@
+/// 数据库驱动
+pub trait RdbcDriver{}
+/// 数据库定义
+pub trait RdbcDataBase{}
+/// 数据源定义
+pub trait RdbcDataSource{}
+/// 数据库连接池定义
+pub trait RdbcPool{}
+/// 数据库连接
+pub trait RdbcConnection{}
+/// 数据库事务
+pub trait RdbcTransaction{}
