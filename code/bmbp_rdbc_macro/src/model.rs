64df20bd69a6f805b00c458c8cd80910b3b6668b
zhangguokai <zhangguokai@inspur.com>
2024-02-20 15:53:35 +0800

    feat: 优化ORM类部局

--- code/bmbp_rdbc_macro/src/model.rs
+++ code/bmbp_rdbc_macro/src/model.rs
@@ -1,8 +1,77 @@
+use std::collections::HashMap;
 use std::fmt::Debug;
 use serde::{Deserialize, Serialize};
+use bmbp_rdbc_sql::RdbcValue;
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(default)]
+pub struct RdbcPage<T> where T: Default + Debug + Clone + Serialize + From<RdbcOrmRow> {
+    page_size: usize,
+    page_num: usize,
+    total: usize,
+    data: Option<Vec<T>>,
+}
+
+impl<T> RdbcPage<T> where T: Default + Debug + Clone + Serialize + From<RdbcOrmRow> {
+    pub fn new() -> Self {
+        RdbcPage {
+            page_size: 10,
+            page_num: 1,
+            total: 0,
+            data: None,
+        }
+    }
+    pub fn new_with_page(page_size: usize, page_num: usize) -> Self {
+        RdbcPage {
+            page_size,
+            page_num,
+            total: 0,
+            data: None,
+        }
+    }
+}
+
+impl<T> RdbcPage<T> where T: Default + Debug + Clone + Serialize + From<RdbcOrmRow> {
+    pub fn page_num(&self) -> &usize {
+        &self.page_num
+    }
+    pub fn page_size(&self) -> &usize {
+        &self.page_size
+    }
+    pub fn total(&self) -> &usize {
+        &self.total
+    }
+    pub fn data(&self) -> &Option<Vec<T>> {
+        &self.data
+    }
+    pub fn set_page_num(&mut self, page_num: usize) -> &mut Self {
+        self.page_num = page_num;
+        self
+    }
+    pub fn set_page_size(&mut self, page_size: usize) -> &mut Self {
+        self.page_size = page_size;
+        self
+    }
+    pub fn set_total(&mut self, total: usize) -> &mut Self {
+        self.total = total;
+        self
+    }
+    pub fn set_data(&mut self, data: Option<Vec<T>>) -> &mut Self {
+        self.data = data;
+        self
+    }
+}
+
+pub struct RdbcOrmRow {
+    columns: Vec<String>,
+    data: HashMap<String, RdbcValue>,
+}
+
 
 /// RdbcTree 定义树型抽象
-pub trait RdbcTree {}
+pub trait RdbcTree<T> {
+}
 
 /// RdbcTreeNode 定义实际树节点
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
@@ -32,14 +101,12 @@ pub struct RdbcTreeNode<T> where T: Default + Debug + Clone + Serialize {
 }
 
 /// RdbcTableModel 数据库表结构抽象
-pub trait RdbcTableModel {
+pub trait RdbcModel {
     fn get_table_name() -> String;
     fn get_table_fields() -> Vec<String>;
-}
-
-/// RdbcTablePrimaryKey 数据库表主键抽象
-pub trait RdbcTablePrimaryKey {
-    fn get_table_primary_key() -> String;
+    fn get_table_primary_key() -> String {
+        "data_id".to_string()
+    }
     fn get_table_union_primary_key() -> Vec<String> {
         return vec![];
     }
@@ -49,7 +116,7 @@ pub trait RdbcTablePrimaryKey {
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 #[serde(default)]
-pub struct RdbcTableRow<T> where T: Default + Debug + Clone + Serialize + RdbcTableModel {
+pub struct RdbcRowModel<T> where T: Default + Debug + Clone + Serialize + RdbcModel {
     /// 记录主键
     data_id: Option<String>,
     /// 记录密级
@@ -78,14 +145,12 @@ pub struct RdbcTableRow<T> where T: Default + Debug + Clone + Serialize + RdbcTa
     ext_props: T,
 }
 
-impl<T> RdbcTablePrimaryKey for RdbcTableRow<T> where T: Default + Debug + Clone + Serialize + RdbcTableModel {
-    fn get_table_primary_key() -> String {
-        return "data_id".to_string();
+impl<T> RdbcModel for RdbcRowModel<T> where T: Default + Debug + Clone + Serialize + RdbcModel {
+    fn get_table_name() -> String {
+        T::get_table_name()
     }
-}
 
-impl<T> RdbcTableRow<T> where T: Default + Debug + Clone + Serialize + RdbcTableModel {
-    pub fn get_table_fields() -> Vec<String> {
+    fn get_table_fields() -> Vec<String> {
         let mut row_fields = vec![
             "data_id".to_string(),
             "data_level".to_string(),
@@ -106,12 +171,11 @@ impl<T> RdbcTableRow<T> where T: Default + Debug + Clone + Serialize + RdbcTable
     }
 }
 
-
 /// /// 定义数据库表-树型结构实体
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 #[serde(default)]
-pub struct RdbcTreeTableRow<T> where T: Default + Debug + Clone + Serialize + RdbcTableModel {
+pub struct RdbcTreeRowModel<T> where T: Default + Debug + Clone + Serialize + RdbcModel {
     /// 记录主键
     data_id: Option<String>,
     /// 记录密级
@@ -147,7 +211,7 @@ pub struct RdbcTreeTableRow<T> where T: Default + Debug + Clone + Serialize + Rd
     // 节点路径名称
     name_path: String,
     // 子节点
-    children: Vec<RdbcTreeTableRow<T>>,
+    children: Vec<RdbcTreeRowModel<T>>,
     // 节点类型
     node_type: Option<String>,
     // 节点层级
@@ -158,13 +222,7 @@ pub struct RdbcTreeTableRow<T> where T: Default + Debug + Clone + Serialize + Rd
     ext_props: T,
 }
 
-impl<T> RdbcTablePrimaryKey for RdbcTreeTableRow<T> where T: Default + Debug + Clone + Serialize + RdbcTableModel {
-    fn get_table_primary_key() -> String {
-        return "data_id".to_string();
-    }
-}
-
-impl<T> RdbcTreeTableRow<T> where T: Default + Debug + Clone + Serialize + RdbcTableModel {
+impl<T> RdbcTreeRowModel<T> where T: Default + Debug + Clone + Serialize + RdbcModel {
     pub fn get_table_fields() -> Vec<String> {
         let mut row_fields = vec![
             "data_id".to_string(),
