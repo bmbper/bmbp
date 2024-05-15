847f529ec227652d164310f2dde434ea28e6cdbc
zhangguokai <zhangguokai@inspur.com>
2024-05-15 21:03:50 +0800

    feat：采用宏自动生成代码

--- code/bmbp_app_rbac/src/role/model.rs
+++ code/bmbp_app_rbac/src/role/model.rs
@@ -2,8 +2,8 @@ use ::serde::Deserialize;
 use ::serde::Serialize;
 use bmbp_app_common::*;
 use bmbp_rdbc_marco::rdbc_model;
+use bmbp_rdbc_orm::RdbcOrmRow;
 use bmbp_rdbc_orm::*;
-use bmbp_rdbc_orm::{BmbpRdbcModel, RdbcModel, RdbcOrmRow};
 use chrono::Utc;
 use salvo::*;
 use tracing::*;
@@ -11,6 +11,7 @@ use tracing::*;
 #[rdbc_model(table = BMBP_RBAC_ROLE)]
 pub struct BmbpRbacRole {
     role_code: Option<String>,
+    #[query(like)]
     role_name: Option<String>,
     role_desc: Option<String>,
 }
