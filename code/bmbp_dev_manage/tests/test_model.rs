20b257a8d242c78bf802fef43c0ec537503d9bfb
zhangguokai <zhangguokai@inspur.com>
2024-06-07 16:41:35 +0800

    fix: 单元测试

--- code/bmbp_dev_manage/tests/test_model.rs
+++ code/bmbp_dev_manage/tests/test_model.rs
@@ -2,5 +2,6 @@ use bmbp_dev_manage::BmbpDevTable;
 
 #[test]
 fn test_model() {
-    let table = BmbpDevTable::default();
+    let _ = BmbpDevTable::default();
+    assert_eq!(BmbpDevTable::get_rdbc_table_name(), "BMBP_DEV_TABLE")
 }
