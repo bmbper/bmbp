3b4f0fb186df2d9f7c280382467aee04ba267054
zhangguokai <zhangguokai@inspur.com>
2024-07-16 21:15:17 +0800

    init: 增加Bootstrap界面

--- code/bmbp_app_home/src/router.rs
+++ code/bmbp_app_home/src/router.rs
@@ -1,6 +1,14 @@
 use crate::home::home_view;
+use rust_embed::RustEmbed;
+use salvo::serve_static::static_embed;
 use salvo::Router;
-
+#[derive(RustEmbed)]
+#[folder = "script"]
+struct HomeScriptAsset;
 pub fn build_app_home_router() -> Router {
-    Router::with_path("/").push(Router::with_path("/home").get(home_view))
+    let static_router =
+        Router::with_path("/ui/lib/home/<**path>").get(static_embed::<HomeScriptAsset>());
+    let mut router = Router::with_path("/").push(Router::with_path("/home").get(home_view));
+    router = router.push(static_router);
+    router
 }
