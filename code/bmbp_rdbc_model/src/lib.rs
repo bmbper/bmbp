7891e6cdaf858b9335d61217bcb55d0c2f573608
zhangguokai <zhangguokai@inspur.com>
2024-07-23 21:01:13 +0800

    feat: 定义BmbpRdbcTable接口

--- code/bmbp_rdbc_model/src/lib.rs
+++ code/bmbp_rdbc_model/src/lib.rs
@@ -8,3 +8,10 @@ mod model;
 mod tree;
 mod tree_util;
 mod valid;
+
+pub trait BmbpRdbcTable {
+    fn get_table_name() -> String;
+    fn get_table_columns() -> Vec<String>;
+    fn get_table_primary_key() -> String;
+    fn get_table_union_primary_key() -> Vec<String>;
+}
