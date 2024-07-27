10f14b433b5a0e73b6aec91ccbb4d5e7f65b8bef
zhangguokai <zhangguokai@inspur.com>
2024-07-27 17:05:28 +0800

    调整文件后缀名

--- code/bmbp_app_home/src/view/home.rs
+++ code/bmbp_app_home/src/view/home.rs
@@ -5,5 +5,5 @@ use salvo::{handler, Request, Response};
 #[handler]
 pub async fn home_view(req: &mut Request, res: &mut Response) {
     let mut ctx = tera::Context::new();
-    res.render(Text::Html(home_tera.render("home.bpg", &ctx).unwrap()));
+    res.render(Text::Html(home_tera.render("home.btl", &ctx).unwrap()));
 }
