9ca714a498568e543a358684b77975146b6679a0
zhangguokai <zhangguokai@inspur.com>
2024-07-28 22:17:21 +0800

    feat:初始化页面增加表单属性

--- code/bmbp_app_init/lib/js/init.js
+++ code/bmbp_app_init/lib/js/init.js
@@ -14,24 +14,54 @@ const InitView = () => {
   }, formTitle, formBody);
 }
 
-
 const FormTitle = () => {
   const h1 = React.createElement('h1', {}, '系统初化配置');
   return React.createElement('div', {}, h1);
 }
 const FormBody = () => {
   let formCard = [
-    React.createElement(arco.Card, { className: "form-card", title: '应用基本信息' }, null),
+    React.createElement(BaseFormCard, {}, null),
     React.createElement(arco.Card, { className: "form-card", title: '应用服务信息' }, null),
     React.createElement(arco.Card, { className: "form-card", title: '数据源信息' }, null),
     React.createElement(FormBottomBar, { className: "form-card", }, null),
   ];
-
-  return React.createElement(arco.Form, {}, formCard);
+  window.formRef = React.useRef();
+  return React.createElement(arco.Form, {
+    ref: window.formRef
+  }, formCard);
 }
+
+const BaseFormCard = () => {
+  let fields = [
+    React.createElement(arco.Form.Item, { label: '应用编码', field: 'code' }, React.createElement(arco.Input, { palaceHolder: '请输入应用名称' }, null)),
+  ];
+
+  return React.createElement(arco.Card, { className: "form-card", title: '应用基本信息' }, ...fields);
+
+}
+
 const FormBottomBar = () => {
-  const okBtn = React.createElement(arco.Button, {}, '保存');
-  const cancelBtn = React.createElement(arco.Button, {}, '取消');
+  const okBtn = React.createElement(arco.Button, {
+    type: 'primary', onClick: (e) => {
+      window.formRef.current.validate().then((values) => {
+        saveFormData(values);
+      })
+    }
+  }, '保存');
+  const cancelBtn = React.createElement(arco.Button, {
+    onClick: () => {
+      clearFormData();
+    }
+  }, '取消');
   const space = React.createElement(arco.Space, {}, okBtn, cancelBtn)
   return React.createElement('div', {}, space);
+}
+
+/// 保存配置信息数据
+const saveFormData = (data) => {
+  arco.Message.info(JSON.stringify(data));
+}
+/// 清空表单数据
+const clearFormData = () => {
+  window.formRef.current.resetFields();
 }
