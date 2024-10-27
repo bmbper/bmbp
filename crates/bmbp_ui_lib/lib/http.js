5564d5fe81a2118d44bdd6f248a1e985093c2ad8
zhangguokai <zhangguokai@inspur.com>
2024-10-27 22:52:37 +0800

    init: 升级静态文件

--- crates/bmbp_ui_lib/lib/http.js
+++ crates/bmbp_ui_lib/lib/http.js
@@ -1,35 +1,30 @@
-// 添加请求拦截器
-axios.interceptors.request.use(
-  function (config) {
-    // 在发送请求之前做些什么
-    debugger;
-    if (!config.headers) {
-      config.headers = { "content-type": "application/json" };
-    } else if (!config.headers["content-type"]) {
-      config.headers["content-type"] = "application/json";
-    }
-    return config;
-  },
-  function (error) {
-    // 对请求错误做些什么
-    return Promise.reject(error);
-  },
-);
-
-axios.interceptors.response.use(
-  function (response) {
-    let data = response.data;
-    if (data.error) {
-      return {
-        code: data.error.code,
-        msg: data.error.name,
-        data: data.error,
-      };
-    } else {
-      return data;
-    }
-  },
-  function (error) {
-    return Promise.reject(error);
-  },
-);
+// web/tsx/http.tsx
+axios.interceptors.request.use(function(config) {
+  if (!config.headers) {
+    config.headers = { "content-type": "application/json" };
+  } else if (!config.headers["content-type"]) {
+    config.headers["content-type"] = "application/json";
+  }
+  return config;
+}, function(error) {
+  return Promise.reject(error);
+});
+axios.interceptors.response.use(function(response) {
+  let data = response.data;
+  if (data.error) {
+    return {
+      code: data.error.code,
+      msg: data.error.name,
+      data: response.config.url
+    };
+  } else {
+    return data;
+  }
+}, function(error) {
+  let errJson = error.toJSON();
+  return {
+    code: errJson.status,
+    msg: errJson.msg,
+    data: errJson.code
+  };
+});
