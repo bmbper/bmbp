f3b4009d0f4839a17c96dc7ea180bd9e76a71914
zhangguokai <zhangguokai@inspur.com>
2024-11-29 23:06:23 +0800

    feat: 调整树型结构

--- crates/bmbp_app_orm/src/lib.rs
+++ crates/bmbp_app_orm/src/lib.rs
@@ -1,1 +1,17 @@
+use bmbp_orm::{RdbcDataSource, RdbcDbType, RdbcOrm};
+use std::sync::{Arc, LazyLock};
 
+pub static BMBP_APP_ORM: LazyLock<RdbcOrm> = LazyLock::new(|| {
+    // TODO BMBP_VARS
+    let ds = RdbcDataSource {
+        db_type: RdbcDbType::Mysql,
+        host: "".to_string(),
+        port: 0,
+        user: "".to_string(),
+        password: "".to_string(),
+        db_name: "".to_string(),
+        charset: "".to_string(),
+        pool_config: Default::default(),
+    };
+    RdbcOrm::new(Arc::new(ds)).unwrap()
+});
