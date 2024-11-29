128682e1dff9fc66507d57a2723237e29f228869
zhangguokai <zhangguokai@inspur.com>
2024-11-29 17:42:21 +0800

    feat: 替换UTIL模块

--- crates/bmbp_app_preview/src/lib.rs
+++ crates/bmbp_app_preview/src/lib.rs
@@ -1,16 +1,1 @@
-use salvo::Router;
 
-mod file;
-mod tmpl;
-mod view;
-
-use crate::view::file_view;
-use file::file_router;
-
-pub fn build_file_router() -> Router {
-    Router::new().push(
-        Router::with_path("bmbp/file")
-            .push(Router::with_path("manage").get(file_view))
-            .push(file_router()),
-    )
-}
